d61d1aae971e32a01e7462d9334064acbe2f841f
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2011-05-21 09:49:20 +0200

    replicated to finish

--- finish/project/build/Project.scala
+++ finish/project/build/Project.scala
@@ -1,6 +1,8 @@
 import sbt._
 
-class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject
-{
+class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
  override def repositories = Set("akka" at "http://akka.io/repository")
+ val akkaHttp = akkaModule("http")
+ val akkaCamel = akkaModule("camel")
 }
+
