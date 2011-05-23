87be6e6ba915a5f32901d9838d739a78f61b213b
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2011-05-23 14:12:47 +0200

    Added eclipsify sbt plugin

--- start/project/build/Project.scala
+++ start/project/build/Project.scala
@@ -1,7 +1,10 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProject {
+class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with AkkaProject {
  override def repositories = Set("akka" at "http://akka.io/repository")
+ val akkaKernel = akkaModule("kernel")
+ val akkaRemote = akkaModule("remote")
  val akkaHttp = akkaModule("http")
  val akkaCamel = akkaModule("camel")
 }
