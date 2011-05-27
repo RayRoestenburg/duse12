c9bd1384d6aa1e119cd00315b6db17a76155074f
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2011-05-27 09:23:14 +0200

    using akka kernel project in Project.scala

--- start/project/build/Project.scala
+++ start/project/build/Project.scala
@@ -1,11 +1,11 @@
 import sbt._
 import de.element34.sbteclipsify._
 
-class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with AkkaProject {
+class JunctionProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with AkkaKernelProject {
  override def repositories = Set("akka" at "http://akka.io/repository")
- val akkaKernel = akkaModule("kernel")
  val akkaRemote = akkaModule("remote")
  val akkaHttp = akkaModule("http")
  val akkaCamel = akkaModule("camel")
+ lazy val logback = "ch.qos.logback" % "logback-classic" % "0.9.28" % "runtime"
 }
 
