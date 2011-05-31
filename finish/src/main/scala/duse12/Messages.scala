cb9ce3cdab5751cd84dc97dcdb0be27af50eff13
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2011-05-31 09:19:59 +0200

    removed unused messages

--- finish/src/main/scala/duse12/Messages.scala
+++ finish/src/main/scala/duse12/Messages.scala
@@ -8,101 +8,66 @@ package messages {
 import java.util.concurrent.atomic.AtomicInteger
 
 object MsgUtils {
-    private val counter = new AtomicInteger()
-    def newDate = new Date(System.currentTimeMillis())
-    def genNextId = counter.incrementAndGet()
+  private val counter = new AtomicInteger()
 
-  }
-  import MsgUtils._
+  def newDate = new Date(System.currentTimeMillis())
 
-  trait JunctionEvent {
+  def genNextId = counter.incrementAndGet()
 
-  }
-  /**
-   * message sent to the sensor that a vehicle is detected,
-   * crossedMarker indicates if the vehicle crossed the marker on the road (passed the sensor)
-   * or if it is approaching the junction
-   */
-  case class VehicleDetected(id: Int, crossedMarker: Boolean = false, timestamp: Date = newDate) extends JunctionEvent
+}
 
-  /**
-   * message sent from sensor to junction, that a vehicle is queueCount on the lane
-   * and that should be sent to the TrafficLight (for display purposes).
-   */
-  case class VehicleQueued(id: Int, lane: LANE.HEADING, queueCount:Int, timestamp: Date = newDate) extends JunctionEvent
+import MsgUtils._
 
-  case class CountVehiclesRequest()
+trait JunctionEvent {
 
-  /**
-   * message sent from the sensor to the junction that a vehicle has passed the traffic light
-   */
-  case class VehiclePassed(id: Int, lane: LANE.HEADING, queueCount:Int, timestamp: Date = newDate) extends JunctionEvent
+}
 
-  /**
-   * Command to switch a traffic light on a lane to green.
-   * If a TrafficLight gets a GreenLight signal for another lane, it should switch to red.
-   */
-  case class GreenLight(lane: LANE.HEADING)
+/**
+ * message sent to the sensor that a vehicle is detected,
+ * crossedMarker indicates if the vehicle crossed the marker on the road (passed the sensor)
+ * or if it is approaching the junction
+ */
+case class VehicleDetected(id: Int, crossedMarker: Boolean = false, timestamp: Date = newDate) extends JunctionEvent
 
-  /**
-   * Command to control traffic, which should be sent by a scheduler to the junction.
-   */
-  case class ControlTraffic()
+/**
+ * message sent from sensor to junction, that a vehicle is queueCount on the lane
+ * and that should be sent to the TrafficLight (for display purposes).
+ */
+case class VehicleQueued(id: Int, lane: LANE.HEADING, queueCount: Int, timestamp: Date = newDate) extends JunctionEvent
 
-  /**
-   * Resets the Junction
-   */
-  case class ResetJunction() extends JunctionEvent
+case class CountVehiclesRequest()
 
-  /**
-   * The decision the junction has made at a specific time, result of ControlTraffic command.
-   */
-  case class JunctionDecision(lane: LANE.HEADING) extends JunctionEvent
+/**
+ * message sent from the sensor to the junction that a vehicle has passed the traffic light
+ */
+case class VehiclePassed(id: Int, lane: LANE.HEADING, queueCount: Int, timestamp: Date = newDate) extends JunctionEvent
+
+/**
+ * Command to switch a traffic light on a lane to green.
+ * If a TrafficLight gets a GreenLight signal for another lane, it should switch to red.
+ */
+case class GreenLight(lane: LANE.HEADING)
+
+/**
+ * Command to control traffic, which should be sent by a scheduler to the junction.
+ */
+case class ControlTraffic()
+
+/**
+ * Resets the Junction
+ */
+case class ResetJunction() extends JunctionEvent
+
+/**
+ * The decision the junction has made at a specific time, result of ControlTraffic command.
+ */
+case class JunctionDecision(lane: LANE.HEADING) extends JunctionEvent
 
 
-  case class DecisionsRequest()
-  case class DecisionsResponse(history:List[JunctionEvent])
+case class DecisionsRequest()
 
-/* TODO implement these in queries and commands
-  /**
-   * Query a traffic light
-   */
-  case class QueryTrafficLight(lane: LANE.HEADING)
+case class DecisionsResponse(history: List[JunctionEvent])
 
-  /**
-   * Query all traffic lights
-   */
-  case class QueryAllTrafficLights()
 
-  /**
-   * Query all sensors
-   */
-  case class QueryAllSensors()
-
-  /**
-   * Query a sensor on a lane
-   */
-  case class QuerySensor(lane: LANE.HEADING)
-
-  /**
-   * Query the junction
-   */
-  case class QueryJunction()
-
-  /**
-   * View of how long a vehicle has waited at the junction
-   */
-  case class VehicleData(id: Int, lane: LANE.HEADING, waitTime: Duration)
-
-  /**
-   * View of how long the TrafficLight has been on green, orange and red
-   */
-  case class TrafficLightData(lane: LANE.HEADING, timeGreen: Duration, timeOrange: Duration, timeRed: Duration, timeOff: Duration)
-
-  /**
-   * View of how many vehicles were detected by the sensor
-   */
-  case class SensorData(lane: LANE.HEADING, countDetected: Int)
-*/
 }
 }
