785ef83a9b05b190811c75b6c639bb7cd74ed5be
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2011-06-13 11:39:13 +0200

    removed bivvy

--- start/project/plugins/Plugins.scala
+++ start/project/plugins/Plugins.scala
@@ -7,7 +7,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
   val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
-  val bumRepo = "Bum Networks Release Repository" at "http://repo.bumnetworks.com/releases"
-  val sbtAkkaBivy = "net.evilmonkeylabs" % "sbt-akka-bivy" % "0.2.0"
   val formatter = "com.github.olim7t" % "sbt-scalariform" % "1.0.3"
 }
